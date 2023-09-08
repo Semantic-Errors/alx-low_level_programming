a737ee9f184f06d045aa749d169501541b84a99e
Semantic-Errors <buggyc.qz6o3@silomails.com>
2023-09-08 04:06:43 +0300

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints a statement to the screen
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}

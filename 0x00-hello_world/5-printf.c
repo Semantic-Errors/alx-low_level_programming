b36620db2aa6b94672017981cfb32afcb4fa4c83
Semantic-Errors <buggyc.qz6o3@silomails.com>
2023-09-08 04:11:04 +0300

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints statement to the screen
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}

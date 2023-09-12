f3fc387bda59c853fd1ac1ac7de45d8fcad1bbc2
Semantic-Errors <buggyc.qz6o3@silomails.com>
2023-09-13 02:44:41 +0300

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function that prints every minute of the day of Jack Bauer,
+ *							starting from 00:00 to 23:59
+ *
+ * Return: Nothing
+ */
+
+void jack_bauer(void)
+{
+	int hour, minute;
+
+
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar('0' + ((hour / 10) % 10));
+			_putchar('0' + (hour % 10));
+
+			_putchar(':');
+
+			_putchar('0' + ((minute / 10) % 10));
+			_putchar('0' + (minute % 10));
+
+			_putchar('\n');
+		}
+	}
+}

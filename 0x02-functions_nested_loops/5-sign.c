0025166c15c99f1fb94c8fe720c6eaa6f78b8c32
Semantic-Errors <buggyc.qz6o3@silomails.com>
2023-09-13 01:43:21 +0300

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,9 +15,17 @@ int print_sign(int n)
 	int sign_code = 0;
 
 	if (n > 0)
+	{
+		_putchar('+');
 		sign_code = 1;
+	}
 	else if (n < 0)
+	{
+		_putchar(-);
 		sign_code = -1;
-
+	}
+	else
+		_putchar('0');
+	
 	return (sign_code);
 }

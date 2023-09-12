c3519ab62031b2035e435304804a295f4b97f0fa
Semantic-Errors <buggyc.qz6o3@silomails.com>
2023-09-13 01:26:50 +0300

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - function that checks for alphabetic character
+ *
+ * @c: character to be checked
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase
+ *                      0 otherwise
+ */
+
+int _isalpha(int c)
+{
+    int is_lower_char = ('a' <= c && c <= 'z');
+    int is_upper_char = ('A' <= c && c <= 'Z');
+
+    return (is_lower_char || is_upper_char);
+}

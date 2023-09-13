ebd58549e87d3faf35fcba64db5b1a38883c00c9
Semantic-Errors <buggyc.qz6o3@silomails.com>
2023-09-13 03:49:19 +0300

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result
+ *
+ * @num_one: first integer to be added
+ * @num_two: second integer to be added
+ *
+ * Return: the result of the addition
+ */
+
+int add(int num_one, int num_two)
+{
+	return (num_one + num_two);
+}

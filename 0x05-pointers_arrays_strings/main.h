9a9ed329b8658def277ee03b1c3fa32da5da9d81
Semantic-Errors <buggyc.qz6o3@silomails.com>
2023-09-20 00:10:26 +0300

    0. 98 Battery st

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef ALX_0X05_POINTERS_ARRAYS_STRINGS_MAIN_H_
+#define	ALX_0X05_POINTERS_ARRAYS_STRINGS_MAIN_H_
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+#endif /* ALX_0X05_POINTERS_ARRAYS_STRINGS_MAIN_H_ */

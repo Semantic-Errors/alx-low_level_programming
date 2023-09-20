04d532416ec01ba8f6086d1fd6d2ae2ab53b8212
Semantic-Errors <buggyc.qz6o3@silomails.com>
2023-09-20 04:39:01 +0300

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * sizeofstring - function that returns the size of string
+ *
+ * @s: pointer to a string that want to return its size
+ *
+ * Return: the length of the string
+*/
+
+int sizeofstring(char *s)
+{
+	int lenght;
+
+	for (lenght = 0; *s != '\0'; s++, lenght++)
+		;
+
+	return (lenght);
+}
+
+/**
  * puts2 - function that prints every other character of a string,
  *				starting with the first character, followed by a new line.
  *
@@ -11,7 +29,11 @@
 
 void puts2(char *str)
 {
-	for (; *str != '\0'; str += 2)
-		_putchar(*str);
+	int i, length;
+
+	length = sizeofstring(str);
+
+	for (i = 0; i < length; i += 2)
+		_putchar(str[i]);
 	_putchar('\n');
 }

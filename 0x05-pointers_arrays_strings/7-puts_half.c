8b2145abee3b1c0c0993d5657020f9abd5a0fc07
Semantic-Errors <buggyc.qz6o3@silomails.com>
2023-09-20 03:29:38 +0300

    7. Winning is only half of it. Having fun is the other half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * sizeofstring - function that returns the size of string
+ *
+ * @s: pointer to a string that want to return its size
+ *
+ * Return: the length of the string
+*/
+
+int sizeofstring(char *s)
+{
+	int lenght;
+
+	for (lenght = 0; *s != '\0'; s++, lenght++)
+		;
+
+	return (lenght);
+}
+
+/**
+ * puts_half - function that prints half of a string, followed by a new line
+ *
+ * @str: string to be print its half
+ *
+ * Return: Nothing
+ */
+
+void puts_half(char *str)
+{
+	int index = (sizeofstring(str)) / 2;
+
+	for (; str[index] != '\0'; index++)
+		_putchar(str[index]);
+	_putchar('\n');
+}

db43cdcc9922327ed1d53e2f765ddab8060ea2fb
Semantic-Errors <buggyc.qz6o3@silomails.com>
2023-09-20 03:48:42 +0300

    8. Arrays are not pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,8 @@
 #include "main.h"
 
 /**
- * print_array - function that prints n elements of an array of integers, followed by a new line
+ * print_array - function that prints n elements of an array of integers,
+ *						followed by a new line
  *
  * @a: array to be print its elements
  * @n: number of elements to be printed

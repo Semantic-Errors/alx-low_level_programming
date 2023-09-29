17412622169bb80a74719c7e49ecc9ccac470ea8
Semantic-Errors <buggyc.qz6o3@silomails.com>
2023-09-29 03:30:11 +0300

    7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,5 +9,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int is_palindrome(char *s);
 
 #endif /* ALX_0X08_RECURSION_MAIN_H_ */

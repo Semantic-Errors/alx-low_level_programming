4c2a42d0a437d2eee3468d166163ac4d5ead5e24
Semantic-Errors <buggyc.qz6o3@silomails.com>
2023-10-24 10:27:14 +0300

    2. Add node

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_nodeint - function that adds a new node at the beginning of a list
+ *
+ * @head: the head of list
+ * @n: the value of the node to be added to the beginning
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node = malloc(sizeof(*head));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *(head);
+
+	return (*head = new_node);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -20,5 +20,6 @@ typedef struct listint_s
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif /* ALX_0X13_MORE_SINGLY_LINKED_LISTS_LISTS_H_ */

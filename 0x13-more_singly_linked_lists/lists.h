a2b3d225669c5f4a8bd0d1d77c74c6ead60a07a5
Semantic-Errors <buggyc.qz6o3@silomails.com>
2023-10-24 10:52:35 +0300

    3. Add node at the end

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define ALX_0X13_MORE_SINGLY_LINKED_LISTS_LISTS_H_
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -21,5 +22,6 @@ int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 
 #endif /* ALX_0X13_MORE_SINGLY_LINKED_LISTS_LISTS_H_ */

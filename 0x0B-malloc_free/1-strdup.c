0bec1424baf3a022493151afd625fb7fcef66043
Semantic-Errors <buggyc.qz6o3@silomails.com>
2023-11-24 20:17:21 +0200

    1. The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,5 +1,25 @@
 #include "main.h"
-#include <string.h>
+
+/**
+ * str_len - function that returns the size of a given string
+ *
+ * @str: the string to get its size
+ *
+ * Return: the size of the string or 0 if string is empty or NULL
+ */
+
+unsigned int str_len(char *str)
+{
+	unsigned int size = 0;
+
+	if (str == NULL)
+		return (0);
+
+	while (str[size] != '\0')
+		size++;
+
+	return (size);
+}
 
 /**
  * _strdup - function that returns a pointer to a newly allocated space in
@@ -16,12 +36,15 @@ char *_strdup(char *str)
 	char *new_str;
 	unsigned int i, size;
 
-	size = strlen(str);
+	size = str_len(str);
 
 	if (size == 0)
 		return (NULL);
 
 	new_str = malloc((size + 1) * sizeof(char));
+
+	if (new_str == NULL)
+		return (NULL);
 
 	for (i = 0; i <= size; i++)
 		new_str[i] = str[i];

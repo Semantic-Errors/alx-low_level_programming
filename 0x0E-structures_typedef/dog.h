3f6ae985b50d7de0587194c4ac5756500dc27aca
Semantic-Errors <buggyc.qz6o3@silomails.com>
2023-11-27 23:57:34 +0200

    0x0E. C - Structures, typedef - 0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef ALX_0X0E_STRUCTURES_TYPEDEF_DOG_H_
+#define ALX_0X0E_STRUCTURES_TYPEDEF_DOG_H_
+
+/**
+ * struct dog - stores data about the dog
+ *
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: the name of the dog's owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif /* ALX_0X0E_STRUCTURES_TYPEDEF_DOG_H_ */

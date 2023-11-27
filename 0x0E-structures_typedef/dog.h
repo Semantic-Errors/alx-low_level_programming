ba7c502bd34a99b860656f23d1bf8c5633a3730e
Semantic-Errors <buggyc.qz6o3@silomails.com>
2023-11-28 01:19:56 +0200

    3. Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,13 +9,19 @@
  * @age: age of the dog
  * @owner: the name of the dog's owner
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 #endif /* ALX_0X0E_STRUCTURES_TYPEDEF_DOG_H_ */

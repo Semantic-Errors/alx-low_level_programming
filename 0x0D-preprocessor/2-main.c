1d03c8a5d91f867ae8e45bac0363dfd2ab5d3bf1
Semantic-Errors <buggyc.qz6o3@silomails.com>
2023-11-29 01:18:33 +0200

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the name of the file it was compiled
+ *
+ * Return: Norhing
+ */
+
+int main()
+{
+	printf("%s\n", __FILE__);
+
+	return (EXIT_SUCCESS);
+}

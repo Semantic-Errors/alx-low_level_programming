36490cb4ba9058abdd69b2fdf73893b70e91c0cb
Semantic-Errors <buggyc.qz6o3@silomails.com>
2023-11-29 23:01:07 +0200

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ *
+ * @name: the name to be printed
+ * @f: function pointer to be print the name with (special printing)
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef ALX_0X0F_FUNCTION_POINTERS_H_
+#define ALX_0X0F_FUNCTION_POINTERS_H_
+
+void print_name(char *name, void (*f)(char *));
+
+#endif /* ALX_0X0F_FUNCTION_POINTERS_H_ */
